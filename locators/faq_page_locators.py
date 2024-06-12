bab0c968111bc384e26b41e411dd1a21a68f4de2
Tatyana Evmenova <45289047+sherhin@users.noreply.github.com>
2024-06-12 19:48:02 +0300

    Delete locators directory

--- locators/faq_page_locators.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class FaqPageLocators:
-
-    QUESTION_LOCATOR = By.XPATH, './/div[@id="accordion__heading-{}"]'
-    ANSWER_LOCATOR = By.XPATH, '//div[@id="accordion__panel-{}"]'
